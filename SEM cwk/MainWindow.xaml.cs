a2591d35fc7a4883638c093003d63db778feb1c0
Dan's Laptop <dkorsah15@aol.co.uk>
2017-03-23 16:49:03 +0000

    Inistial commit. Basic tinkering.

---
+++ SEM cwk/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SEM_cwk
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SignUpButton_Click(object sender, RoutedEventArgs e)
+        {
+            SingUpPage s = new SingUpPage();
+            this.Content = s;
+        }
+
+        private void LogInButton_Click(object sender, RoutedEventArgs e)
+        {
+            SingUpPage L = new SingUpPage();
+            L.textBox.Text = "Fuck You too!";
+            this.Content = L;
+            
+        }
+    }
+}
